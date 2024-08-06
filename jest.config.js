cba26429e7f35268e471ef6fb7ccb4e5bb91f513
MinhazulAbedinMithu <minhazulabedinmithu@gmail.com>
2024-08-07 05:33:19 +0600

    added

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
-
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': './src/$1',
+  },
+  setupFilesAfterEnv: ['./src/setupTests.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!@bundled-es-modules/tough-cookie|msw)',
+  ],
+};
--- src/__test__/SalesforceData.test.tsx
+++ src/__test__/SalesforceData.test.tsx
@@ -1,40 +1,51 @@
-// src/pages/Homepage.test.tsx
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import fetchMock from 'jest-fetch-mock';
 import dataReducer, { fetchData } from '../store/features/dataSlice';
 import Home from '../pages/Home';
 
-fetchMock.enableMocks();
-
-const renderWithProviders = (ui: React.ReactElement, { reduxState } = { reduxState: {} }) => {
-  const store = configureStore({
-    reducer: { data: dataReducer },
-    preloadedState: reduxState,
-  });
-
-  return render(<Provider store={store}>{ui}</Provider>);
+const renderWithStore = (store: ReturnType<typeof configureStore>) => {
+  return render(
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  );
 };
 
-beforeEach(() => {
-  fetchMock.resetMocks();
-});
+describe('Home', () => {
+  let store: ReturnType<typeof configureStore>;
 
-test('fetches and displays data from API', async () => {
-  fetchMock.mockResponseOnce(JSON.stringify({ data: 'Salesforce data' }));
-
-  renderWithProviders(<Home />);
-
-  // Initially shows loading state
-  expect(screen.getByText(/loading/i)).toBeInTheDocument();
-
-  // Wait for the data to be displayed
-  await waitFor(() => {
-    expect(screen.getByText(/Data: Salesforce data/i)).toBeInTheDocument();
+  beforeEach(() => {
+    store = configureStore({
+      reducer: {
+        data: dataReducer,
+      },
+    });
   });
 
-  // Ensure loading message is no longer present
-  expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
+  it('should display loading state', () => {
+    store.dispatch(fetchData.pending(''));
+    renderWithStore(store);
+    expect(screen.getByText(/Loading.../i)).toBeInTheDocument();
+  });
+
+  it('should display data on successful fetch', async () => {
+    const mockData = { key: 'value' };
+    store.dispatch(fetchData.fulfilled(mockData, ''));
+
+    renderWithStore(store);
+    expect(screen.getByText(/Salesforce data/i)).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByText(/"key": "value"/i)).toBeInTheDocument();
+    });
+  });
+
+  it('should display error message on failed fetch', async () => {
+    const errorMessage = 'Failed to fetch';
+    store.dispatch(fetchData.rejected(new Error(errorMessage), ''));
+
+    renderWithStore(store);
+    expect(screen.getByText(/Error: Failed to fetch/i)).toBeInTheDocument();
+  });
 });
